fee597b7c565b6ed4c539ecb85d09394530c96c4
arko <arkadiusz.kupracz@gmail.com>
2026-07-13 13:10:48 +0100

    extend minigame properties

--- game/scripts/minigames/example_minigame/example_minigame.gd
+++ game/scripts/minigames/example_minigame/example_minigame.gd
@@ -1,16 +1,30 @@
 extends Minigame
 
-const REQUIRED_SCORE := 10
+const _REQUIRED_SCORE := 10
+const _DEFAULT_TIME := 30.0
+const _ALLOWED_TIMES = {
+	0: 10.0,
+	1: 15.0,
+	2: 20.0,
+	3: 25.0,
+	4: 30.0,
+	5: 35.0
+}
 
-var score := 0:
+var _score := 0:
 	set(value):
-		score = value
-		score_label.text = "Score: %s/%s" % [score, REQUIRED_SCORE]
-		if score >= REQUIRED_SCORE:
+		_score = value
+		_score_label.text = "Score: %s/%s" % [_score, _REQUIRED_SCORE]
+		if _score >= _REQUIRED_SCORE:
 			game_won.emit()
 					
-@onready var score_label: Label = %ScoreLabel
+@onready var _score_label: Label = %ScoreLabel
+
+
+func get_time_limit() -> float:
+	return _ALLOWED_TIMES.get(dice_roll, _DEFAULT_TIME)
+			
 
 
 func _on_add_score_button_pressed() -> void:
-	score += 1
+	_score += 1
