671c3ac5f358a6a27ab0548686891f933eeec769
arko <arkadiusz.kupracz@gmail.com>
2026-07-15 20:14:05 +0100

    handle player input for stratagem minigame

--- game/scripts/pc_screen/minigames/stratagem/stratagem.gd
+++ game/scripts/pc_screen/minigames/stratagem/stratagem.gd
@@ -1,5 +1,20 @@
 extends Minigame
 
+enum Direction {
+	RIGHT,
+	DOWN,
+	LEFT,
+	UP
+}
+
+var current_sequence: Array[Direction] = [
+	Direction.RIGHT,
+	Direction.DOWN,
+	Direction.LEFT,
+	Direction.UP
+]
+
+
 @onready var timer_component: TimerComponent = %TimerComponent
 
 
@@ -7,5 +22,26 @@ func _ready() -> void:
 	timer_component.start_timer(get_time_limit())
 
 
+func _input(event: InputEvent) -> void:
+	if not event.is_pressed() or event.is_echo():
+		return
+	if event.is_action(&"ui_right"):
+		_player_action(Direction.RIGHT)
+	elif event.is_action(&"ui_down"):
+		_player_action(Direction.DOWN)
+	elif event.is_action(&"ui_left"):
+		_player_action(Direction.LEFT)
+	elif event.is_action(&"ui_up"):
+		_player_action(Direction.UP)
+	else:
+		return
+	
+	get_viewport().set_input_as_handled()
+
+
 func get_time_limit() -> float:
 	return 100.0
+
+
+func _player_action(direction: Direction) -> void:
+	print(direction)
